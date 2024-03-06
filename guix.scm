13c51267d1249d26095e2dce983eaa146f84b230
Ludovic Courtès <ludo@gnu.org>
2024-03-06 11:42:55 +0100

    maint: Update ‘guix.scm’.
    * guix.scm: Use ‘local-file’ for source.  Remove input labels.  Add
    synopsis, description, and home page.

--- guix.scm
+++ guix.scm
@@ -1,7 +1,9 @@
 (use-modules
+  (guix gexp)
+  (guix utils)
   (guix packages)
   ((guix licenses) #:prefix license:)
-  (guix download)
+  (guix git-download)
   (guix build-system gnu)
   (gnu packages)
   (gnu packages autotools)
@@ -11,20 +13,24 @@
   (gnu packages pkg-config)
   (gnu packages texinfo))
 
+(define vcs-file?
+  ;; Return true if the given file is under version control.
+  (or (git-predicate (current-source-directory))
+      (const #t)))                                ;not in a Git checkout
+
 (package
   (name "guile-zlib")
-  (version "0.1")
-  (source "./guile-zlib-0.1.tar.gz")
+  (version "42.0")
+  (source (local-file "." "guile-zlib-checkout"
+                      #:recursive? #t
+                      #:select? vcs-file?))
   (build-system gnu-build-system)
-  (arguments `())
-  (native-inputs
-    `(("autoconf" ,autoconf)
-      ("automake" ,automake)
-      ("pkg-config" ,pkg-config)))
-  (inputs `(("guile" ,guile-3.0)
-            ("zlib" ,zlib)))
-  (propagated-inputs `())
-  (synopsis "")
-  (description "")
-  (home-page "")
+  (native-inputs (list autoconf automake pkg-config guile-3.0))
+  (inputs (list guile-3.0 zlib))
+  (synopsis "Guile bindings to zlib")
+  (description
+   "This package provides Guile bindings for zlib, a lossless
+data-compression library.  The bindings are written in pure Scheme by using
+Guile's foreign function interface.")
+  (home-page "https://notabug.org/guile-zlib/guile-zlib")
   (license license:gpl3+))
